678f2eb4edf66d6d81e21df69f758e835b229d06
David Kent <davidkent@fastmail.com.au>
2018-09-12 23:10:10 +1000

    Jump start fan.

--- sensor_app/pwm_fan.py
+++ sensor_app/pwm_fan.py
@@ -1,3 +1,5 @@
+import time
+
 from RPi import GPIO
 
 
@@ -24,5 +26,10 @@ class PwmFan:
 
     @duty_cycle.setter
     def duty_cycle(self, value):
+        if value < 60:
+            # Low duty cycle fails to start fan. Give it a burst to get
+            # spinning.
+            self.pwm.ChangeDutyCycle(90)
+            time.sleep(3)
         self.pwm.ChangeDutyCycle(value)
         self._duty_cycle = value
